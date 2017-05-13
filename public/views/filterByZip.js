932f6f81738383e7a865091259d4cc5372a5a9ab
Jamesbillard12 <jamesbillard12@hotmail.com>
2017-05-13 16:14:48 -0700

    fixed bug where it would not show message again

--- public/views/filterByZip.js
+++ public/views/filterByZip.js
@@ -3,21 +3,17 @@
 ((module)=>{
   const filter = {};
 
-  filter.getZip = (event) => {
+
     $('#submit').on('click', function(event) {
       event.preventDefault();
-      if (/^[0-9]{5}$/.test($('#zip').val())) {
-        $('#returnString').remove();
-        let zip = $('#zip').val();
+      let zip = $('#zip').val();
+      if (/^[0-9]{5}$/.test(zip)) {
+        $('#returnString').text('');
         console.log(zip);
-        return zip;
       } else {
         $('#returnString').text("Please enter a five digit zip code.").css("color","red")
       }
-
     })
-  }
   // got the zips
-    filter.getZip()
     module.filter = filter;
   })(window);
