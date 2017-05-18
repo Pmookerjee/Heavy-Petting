24c19beb33f77e5c4a9bbc25b181b693336d5b1d
sgordon834 <sgordon834@gmail.com>
2017-05-17 19:47:59 -0700

    changes

--- public/js/cardTemplate.js
+++ public/js/cardTemplate.js
@@ -8,15 +8,14 @@ const toDom = {};
 toDom.render = Handlebars.compile($('#cardTemplate').html());
 
 toDom.renderToCards = ()=>{
-  console.log("in render to cards");
+  console.log('in render to cards');
   Pets.all.forEach(function(pet) {
     $('#liAppend').append(toDom.render(pet));
   });
 }
 
-module.toDom = toDom;
+  module.toDom = toDom;
 
 })(window);
-
 
 $('input:text').focus(function () {$(this).val('');});
