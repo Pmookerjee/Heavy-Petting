2ced0a8e47a3e3d53a9ab1366a7b36348a9325f6
Jamesbillard12 <jamesbillard12@hotmail.com>
2017-05-19 22:20:14 -0700

    completed styling faves

--- public/views/FavesView.js
+++ public/views/FavesView.js
@@ -2,38 +2,40 @@
 
 (module => {
 
-const Faves = {};
-Faves.all = [];
+  const Faves = {};
+  Faves.all = [];
 
 
-Faves.render = Handlebars.compile($('#favesTemplate').html());
+  Faves.render = Handlebars.compile($('#favesTemplate').html());
 
-Faves.renderFaves = () => {
-   Faves.all = JSON.parse(localStorage.getItem('Likes'));
-  console.log("in render FAVES");
-  Faves.all.forEach(fave => {
-    $('#renderToFaves').append(Faves.render(fave));
+  Faves.renderFaves = () => {
+    Faves.all = JSON.parse(localStorage.getItem('Likes'));
+    console.log("in render FAVES");
+    Faves.all.forEach(fave => {
+      $('#renderToFaves').append(Faves.render(fave));
+    });
+  }
+
+  $('#clearall').on('click', function(event){
+    event.preventDefault();
+    localStorage.clear();
+    window.location.reload(false);
+    page('/faves');
   });
-}
 
-$('#clearall').on('click', function(event){
-  event.preventDefault();
-  localStorage.clear();
-  window.location.reload(false);
-  page('/faves');
-});
+  $('#home').on('click', function(event){
+    event.preventDefault();
+    page('/');
+  });
 
-$('#home').on('click', function(event){
-  event.preventDefault();
-  page('/');
-});
 
-// $('#totop').on('click', function(event){
-//   $('html, body').animate({
-//        scrollTop: $(".favesHead").offset().top
-//    }, 2000);
-// });
 
-module.Faves = Faves;
+  // $('#totop').on('click', function(event){
+  //   $('html, body').animate({
+  //        scrollTop: $(".favesHead").offset().top
+  //    }, 2000);
+  // });
+
+  module.Faves = Faves;
 
 })(window);
