ce8c057aeb428367c565ce04dbb7736027d883a1
Jamesbillard12 <Jamesbillard12@hotmail.com>
2017-05-19 20:08:17 -0700

    I added an x to the faves cards (#32)

--- public/vendor/scripts/jquery.jTinder.js
+++ public/vendor/scripts/jquery.jTinder.js
@@ -184,38 +184,4 @@
 		return this;
 	};
 
-
-	$('#dislike').on('click', function(event){
-		$("#tinderslide").jTinder('dislike');
-	})
-
-	$('#like').on('click', function(event){
-		$("#tinderslide").jTinder('like');
-	})
-
-	$("#tinderslide").jTinder({
-		onDislike: function (item) {
-			// $('#status').html('Pass' + (item.index()+1));
-			$('#status').html('Pass');
-			Pets.saveViewed(item[0].id);
-		},
-		onLike: function (item) {
-			// $('#status').html('Save ' + (item.index()+1) + ' to Favorites');
-			$('#status').html('Saved to Favorites!');
-			var zipStringId = item[0].id;
-			for (var i in Pets.all) {
-				var petsAllIds = Pets.all[i].id;
-				if (zipStringId === (Pets.all[i].id).toString()) {
-				  Pets.saveLike(Pets.all[i]);
-					Pets.saveViewed(Pets.all[i].id);
-				}
-			}
-		},
-		animationRevertSpeed: 200,
-		animationSpeed: 400,
-		threshold: 1,
-		likeSelector: '.like',
-		dislikeSelector: '.dislike'
-	});
-
 })(jQuery, window, document);
