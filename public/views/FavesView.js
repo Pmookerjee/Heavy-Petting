46c0acc4531722823b55578ee673e42df4f384f3
Pmookerjee <pmookerjee@gmail.com>
2017-05-20 02:59:32 -0700

    Jesus (#41)
    * Added an event handler, fixed a missing SaveViewedPets file
    
    * Did some shit that I dont remember now
    
    * fixed merge conflicts

--- public/views/FavesView.js
+++ public/views/FavesView.js
@@ -8,13 +8,14 @@
 
   Faves.render = Handlebars.compile($('#favesTemplate').html());
 
-  Faves.renderFaves = () => {
-    if(localStorage.getItem('Likes') === null){
-      console.log('there is nothing in local storage');
-      let imgPlaceholder = '<img class="empty-faves-pic" src="assets/oops.jpg"></img>';
-      let textPlaceholder = '<p class="empty-faves-text">Looks like you dont have any animals saved yet!</p>';
-      $('#renderToFaves').append(imgPlaceholder);
-      $('#renderToFaves').append(textPlaceholder);
+Faves.renderFaves = () => {
+  console.log('in renderFaves');
+   if(localStorage.getItem('Likes') === null){
+     console.log('there is nothing in local storage');
+     let imgPlaceholder = '<img class="empty-faves-pic" src="assets/oops.jpg"></img>';
+     let textPlaceholder = '<p class="empty-faves-text">Looks like you dont have any animals saved yet!</p>';
+     $('#renderToFaves').append(imgPlaceholder);
+     $('#renderToFaves').append(textPlaceholder);
     } else {
       Faves.all = JSON.parse(localStorage.getItem('Likes'));
       Faves.all.forEach(fave => {
@@ -23,12 +24,29 @@
     }
   }
 
-  $('#clearall').on('click', function(event){
-    event.preventDefault();
-    localStorage.clear();
-    window.location.reload(false);
-    page('/faves');
-  });
+// }
+//}
+
+$('#clearall').on('click', function(event){
+  event.preventDefault();
+  localStorage.clear();
+  window.location.reload(false);
+  page('/faves');
+});
+
+$('#home').on('click', function(event){
+  event.preventDefault();
+  page('/');
+});
+
+$(document).on('click', '.close', function(event){
+  event.preventDefault();
+  let petId = $(this).attr('id');
+  Pets.removeFromLikes(petId);
+  location.reload();
+});
+
+module.Faves = Faves;
 
   $('#home').on('click', function(event){
     event.preventDefault();
